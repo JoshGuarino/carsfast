e35c05d3000d5413222198f643de08e9d8823df6
josh-guarino <josh.guarino@eagleview.com>
2023-08-04 17:28:01 -0400

    add tables to database

--- carsfast/carsfast/settings.py
+++ carsfast/carsfast/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "satellite_monitoring.apps.SatelliteMonitoringConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- carsfast/satellite_monitoring/models.py
+++ carsfast/satellite_monitoring/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Satellites(models.Model):
+    status = models.IntegerField()
+    time_since_change = models.DateTimeField()
+
+class Stats(models.Model):
+    satellite = models.ForeignKey(Satellites, on_delete=models.CASCADE)
+    altitude = models.IntegerField()
+    last_updated = models.DateTimeField()
