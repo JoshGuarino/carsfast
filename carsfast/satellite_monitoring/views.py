25ec5aec49bf5129a7ef7bdca3f8cfff5e1c7a44
josh-guarino <josh.guarino@eagleview.com>
2023-08-04 16:35:22 -0400

    ...

--- carsfast/satellite_monitoring/views.py
+++ carsfast/satellite_monitoring/views.py
@@ -2,18 +2,20 @@ from django.http import JsonResponse, HttpResponse
 import requests
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse('Hello, world.')
 
 
 def stats(request):
     return JsonResponse({
-        "max_altitude": 22, 
-        "min_altitude": 0, 
-        "avg_altitude": 11
+        'max_altitude': 22, 
+        'min_altitude': 0, 
+        'avg_altitude': 11
         })
 
 
 def health(request):
+    response = requests.get('https://satellite.carsfast.workers.dev/')
+
     return JsonResponse({
-        "status": "<insert status here>"
+        'status': '<insert status here>'
         })
